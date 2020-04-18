73a0ff76313c9a39388ec6bc0aed15e19e3c27d8
mbasss <mbasss.ind@gmail.com>
2020-04-18 18:43:02 +0700

    tambah view test

--- index.php
+++ index.php
@@ -43,6 +43,38 @@
             </div>
 
         </div>
+
+        <div class="row mt-5">
+
+            <div class="card bg-dark text-light col-md-6">
+                <div class="card-body">
+                    <h5 class="card-title">Form Import Excel Test</h5>
+                    <form method="post" enctype="multipart/form-data" action="proses/import_test.php">
+                        <div class="form-group">
+                            <label for="exampleInputFile">File Upload</label>
+                            <input type="file" name="berkas_excel" class="form-control-file" id="exampleInputFile">
+                        </div>
+                        <button type="submit" class="btn btn-primary">Import</button>
+                    </form>
+                </div>
+            </div>
+
+            <div class="card bg-dark text-light col">
+                <div class="card-body">
+                    <h5 class="card-title">Form Export Excel Test</h5>
+
+                    <p>File hasil export = htdocs/Data Export/<strong>Data Test.xlsx</strong></p>
+
+                    <div class="alert alert-warning p-0" role="alert">
+                        Data Export otomatis replace !!!
+                    </div>
+
+                    <p></p>
+                    <a class="btn btn-success" href="proses/export_test.php">Export</a>
+                </div>
+            </div>
+
+        </div>
     </div>
 </body>
 
